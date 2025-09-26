64fdef08a8d447d69fce4e47784f6266f3ccdbee
david-rid <davidridgewaygit@gmail.com>
2025-09-26 18:39:02 -0400

    Fixed typos in Car class

--- car.js
+++ car.js
@@ -1,20 +1,19 @@
 class Car {
     constructor(x, y, width, height) {
-        this.x=x;
-        thix.y=y;
-        this.width=width;
-        this.height=height;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
     }
 
     // draw the car method
     draw(ctx) {
         ctx.beginPath();
         ctx.rect(
-            thix.x-this.width/2,
-            this.y-thix.height/2,
-            this.width,
-            this.height
-        );
+            this.x-this.width/2, 
+            this.y-this.height/2, 
+            this.width, 
+            this.height);
         ctx.fill();
     }
 }
