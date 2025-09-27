96b2fa99928ff2fbaae1601016811674ca3bb1d6
david-rid <davidridgewaygit@gmail.com>
2025-09-26 20:17:41 -0400

    Defined the road

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ canvas.width=200;
 
 // create reference to 2d context
 const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width/2, canvas.width*0.9)
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
 function animate() {
     car.update();
-    
+
     canvas.height=window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
