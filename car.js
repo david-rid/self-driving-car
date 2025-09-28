e6914278819ae87877c89e5b08f2988217b1548e
david-rid <davidridgewaygit@gmail.com>
2025-09-28 19:21:43 -0400

    Added sensors to car

--- car.js
+++ car.js
@@ -12,11 +12,14 @@ class Car {
 
         this.angle = 0;
 
+        this.sensor = new Sensor(this);
+
         this.controls =  new Controls();
     }
     
-    update() {
+    update(roadBorders) {
         this.#move();
+        this.sensor.update(roadBorders);
     }
 
     #move() {
@@ -73,5 +76,7 @@ class Car {
         ctx.fill();
 
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
