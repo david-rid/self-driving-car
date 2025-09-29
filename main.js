c9148bc6be021f856ed464a1eab7f2a4f48c5751
david-rid <davidridgewaygit@gmail.com>
2025-09-28 22:08:29 -0400

    Added neural network vizualizer

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ networkCanvas.width=300;
 
 // create reference to 2d context
 const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
 const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 
@@ -23,7 +25,8 @@ function animate() {
 
     car.update(road.borders, traffic);
 
-    carCanvas.height=window.innerHeight;
+    carCanvas.height = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
 
     carCtx.save();
     carCtx.translate(0, -car.y+carCanvas.height*0.7);
@@ -37,6 +40,8 @@ function animate() {
     car.draw(carCtx, "blue");
 
     carCtx.restore();
+
+    Visualizer.drawNetwork(networkCtx, car.brain);
 
     requestAnimationFrame(animate);
 }
